d5313c00c28a28053c45992f6b8d058f02e04a37
Nikhil <n4animeedit@gmail.com>
2025-12-25 22:54:22 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,47 @@
+
+
+
+API_ID = 22299340 # https://my.telegram.org
+API_HASH = "09b09f3e2ff1306da4a19888f614d937"# https://my.telegram.org
+BOT_TOKEN = ""# Form @BotFather
+MONGO_URI = "mongodb+srv://mikota4432:jkJDQuZH6o8pxxZe@cluster0.2vngilq.mongodb.net/?retryWrites=true&w=majority"# Form https://www.mongodb.com
+START_PIC = "https://i.rj1.dev/aNWlA.png" 
+ 
+START_MSG = """<blockquote><b>✨ Hey there!</b></blockquote>
+<blockquote>Need your files sent in the right sequence?</blockquote>
+<blockquote>Send them here, pick a mode, and let me do the work for you.</blockquote>"""
+
+
+HELP_TXT = """<blockquote><b>🤖 ᴍʏ ɴᴀᴍᴇ: ғɪʟᴇ sᴇǫᴜᴇɴᴄᴇ ʙᴏᴛ</b>
+<b>◈ ᴏᴡɴᴇʀ:</b> <a href="https://t.me/N4_Bots"><b>Cʟɪᴄᴋ ʜᴇʀᴇ</b></a><br>
+<b>◈ ʙᴏᴛs ᴄʜᴀɴɴᴇʟ:</b> <a href="https://t.me/N4_Bots"><b>Cʟɪᴄᴋ ʜᴇʀᴇ</b></a><br>
+<b>◈ ᴍᴀɴɢᴀ ᴄʜᴀɴɴᴇʟ:</b> <a href="https://t.me/N4_Bots"><b>Cʟɪᴄᴋ ʜᴇʀᴇ</b></a><br>
+<b>◈ ᴅᴇᴠᴇʟᴏᴘᴇʀ:</b> <a href="https://t.me/N4_Bots"><b>Cʟɪᴄᴋ ʜᴇʀᴇ</b></a>"</blockquote>"""
+
+COMMAND_TXT = """<b>🛠️ Bot Commands List</b>
+
+<blockquote>• /start - Start the bot
+• /sequence - Start file sequencing mode
+• /fileseq - Choose file ordering mode
+• /leaderboard - View top users
+• /sf - Switch between File mode and Caption mode</blockquote>"""
+
+# --- FORCE SUBSCRIBE CONFIG ---
+FSUB_CHANNEL = -1002263636517 #If you don’t want to use it, you can turn it off by setting it to 0
+FSUB_CHANNEL_2 = -1001896877147
+FSUB_CHANNEL_3 = 0 
+
+OWNER_ID = 5380609667
+
+
+
+
+
+
+
+
+
+
+
+
+
